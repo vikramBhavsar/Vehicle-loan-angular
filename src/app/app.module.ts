36a17574276fabc879ca74b28ae6f17db098d619
vikramBhavsar <vikram.bhavsar@somaiya.edu>
2021-08-06 21:42:01 +0530

    UI for Check eligibility component done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EligibityCheckComponent } from './eligibity-check/eligibity-check.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EligibityCheckComponent
   ],
   imports: [
     BrowserModule,
