116fe283da827e3793126e65a054228f524343d5
SuchetBhakhan <suchetbhakhan15@gmail.com>
2021-08-07 15:22:38 +0530

    homechanges&persoanldetailseui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { PersonalDetailsComponent } from './personal-details/personal-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PersonalDetailsComponent
   ],
   imports: [
     BrowserModule,
